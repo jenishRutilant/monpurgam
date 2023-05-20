147cf9932bcdeea871591dc2b90e00e5e0dc8684
umangmalaviya <umang.rutilant@gmail.com>
2023-05-20 14:33:19 +0530

    update - 20230520

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Logo from "../Images/logo.svg"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      {/* header-part */}
+      <header>
+        <div className='header_inn'>
+
+        </div>
+      </header>
+      {/* header-part */}
+    </>
   )
 }
 
